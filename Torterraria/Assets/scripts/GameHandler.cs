288c6089817e88b050905e69c938ac752b2c8096
timothy.peck <timothy.peck@he-arc.ch>
2022-08-25 15:46:48 +0200

    Add enemies to SampleScene (main scene)

--- Torterraria/Assets/scripts/GameHandler.cs
+++ Torterraria/Assets/scripts/GameHandler.cs
@@ -7,7 +7,7 @@ public class GameHandler : MonoBehaviour
     private static int nbEnemies = 2;
 
     private float lastSpawn;
-    public GameObject[] enemiesClones;
+    public GameObject[] enemiesClones = new GameObject[nbEnemies];
     public GameObject player;
 
     void Start()
@@ -22,7 +22,7 @@ public class GameHandler : MonoBehaviour
         {
             // spawn enemy
             int sideSpawn = Random.value < 0.5 ? 10 : -10;
-            Instantiate(enemiesClones[Random.Range(0, enemiesClones.Length)], new Vector2(player.gameObject.transform.position.x + sideSpawn, 4f), Quaternion.identity);
+            Instantiate(enemiesClones[Random.Range(0, enemiesClones.Length)], new Vector2(player.gameObject.transform.position.x + sideSpawn, player.gameObject.transform.position.y + 4f), Quaternion.identity);
             nbEnemies++;
             lastSpawn = Time.time;
         }
